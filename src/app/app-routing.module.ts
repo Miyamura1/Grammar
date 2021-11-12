0a29c990da3a899b11c29731feb1bc7dd518ef45
Miyamura1 <dzulhelmizulkepli@gmail.com>
2021-11-12 11:20:14 +0800

    commit on master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,51 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'verb',
+    loadChildren: () => import('./verb/verb.module').then( m => m.VerbPageModule)
+  },
+  {
+    path: 'pronouns',
+    loadChildren: () => import('./pronouns/pronouns.module').then( m => m.PronounsPageModule)
+  },
+  {
+    path: 'tense',
+    loadChildren: () => import('./tense/tense.module').then( m => m.TensePageModule)
+  },
+  {
+    path: 'adjective',
+    loadChildren: () => import('./adjective/adjective.module').then( m => m.AdjectivePageModule)
+  },
+  {
+    path: 'preposition',
+    loadChildren: () => import('./preposition/preposition.module').then( m => m.PrepositionPageModule)
+  },
+  {
+    path: 'listverb',
+    loadChildren: () => import('./listverb/listverb.module').then( m => m.ListverbPageModule)
+  },
+  {
+    path: 'notes',
+    loadChildren: () => import('./notes/notes.module').then( m => m.NotesPageModule)
+  },
+  {
+    path: 'listexercise',
+    loadChildren: () => import('./listexercise/listexercise.module').then( m => m.ListexercisePageModule)
+  },
+  
 ];
 
 @NgModule({
